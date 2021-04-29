95fdd81c6bb0b20fda10604ab8c9c81420425e29
MADEYE <52187012+seyi91@users.noreply.github.com>
2021-04-29 11:30:26 -0500

    Styled Ravenous webpage with hardcode single restaurant entry

---
+++ ravenous/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import BusinessList from './components/Business List/BusinessList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  )
+}
+
+export default App;
